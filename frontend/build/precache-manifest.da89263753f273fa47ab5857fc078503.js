39a1d918dd860f960dbf3646451d196fabb0f566
bichous <bichous07@gmail.com>
2020-08-27 00:19:27 -0500

    after build

---
+++ frontend/build/precache-manifest.da89263753f273fa47ab5857fc078503.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac9c0ccee4da6a3856daad663b622681",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fefc76ade6cc10cf87d3",
+    "url": "/static/css/main.962ee78a.chunk.css"
+  },
+  {
+    "revision": "59938ecc887a94a8d2a7",
+    "url": "/static/js/2.72d07ac7.chunk.js"
+  },
+  {
+    "revision": "fefc76ade6cc10cf87d3",
+    "url": "/static/js/main.d3f29a67.chunk.js"
+  },
+  {
+    "revision": "fb47f3c02db3ce888eb4",
+    "url": "/static/js/runtime-main.dc128e9d.js"
+  },
+  {
+    "revision": "77ccd3952eb742eec98eef75f243d1b4",
+    "url": "/static/media/portada.77ccd395.png"
+  }
+]);
