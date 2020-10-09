6111bce4cbb9ae9066b4f67a7cc31dc63437315f
Itza Zujuy Blanco <39780663+Zujuy@users.noreply.github.com>
2020-10-09 00:09:20 -0500

    static RRSS bar done

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from "../../images/parvati_logo.png";
-import Aboutus from "../../components/home/Aboutus"
-import Services from "../../components/home/Services"
+import logo from '../../images/parvati_logo.png';
+import Aboutus from '../../components/home/Aboutus';
+import Services from '../../components/home/Services';
 import Testimonies from './Testimonies';
 import Form from './Form';
 import Footer from './Footer';
 import Footer2 from './Designfooter';
 import Carousel from './Carousel2';
 
-
-
 function Home() {
   return (
     <div>
-
+      <div class="icon-bar">
+        <a
+          href="https://www.facebook.com/Parvati-Beauty-Room-103248328161616"
+          target="_blank"
+          class="facebook"
+        >
+          <i class="fa fa-facebook"></i>
+        </a>
+        <a
+          href="https://www.instagram.com/parvati.br/"
+          target="_blank"
+          class="instagram"
+        >
+          <i class="fa fa-instagram"></i>
+        </a>
+        <a href="#" class="whatsapp">
+          <i class="fa fa-whatsapp"></i>
+        </a>
+      </div>
       <div className="home">
         <img src={logo} alt="logo" />
         <div className="hometitle">
           <h2>Es momento de empezar a cuidarte</h2>
         </div>
-      </div >
-
+      </div>
       <div>
         <Carousel />
       </div>
@@ -49,7 +64,6 @@ function Home() {
         <Footer2 />
       </div>
     </div>
-
   );
 }
 
--- frontend/src/style/components.js
+++ frontend/src/style/components.js
@@ -1,14 +1,14 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 //color palet
 export const colors = {
-  text: "#334353",
-  color1: "#FAE8DA",
-  testimoniesback: "#FBC3A7",
-  color2: "#FA8184",
-  copywrite: "#1B2D3F",
-  buttoncarrousel: "#DB0000"
-}
+  text: '#334353',
+  color1: '#FAE8DA',
+  testimoniesback: '#FBC3A7',
+  color2: '#FA8184',
+  copywrite: '#1B2D3F',
+  buttoncarrousel: '#DB0000',
+};
 
 //carousel
 export const Carousel = styled.section`
@@ -20,366 +20,277 @@ overflow-y: scroll;
 padding: 0 2rem;
 
 }
-`
+`;
 //carouselbootstrap
 export const CarouselStyled = styled.section`
-width:100vw;
-display: flex;
-flex-wrap: nowrap;
-flex-direction: row;
-overflow-y: scroll;
-padding: 0;
+  width: 100vw;
+  display: flex;
+  flex-wrap: nowrap;
+  flex-direction: row;
+  overflow-y: scroll;
+  padding: 0;
 
-.boton1{
-  margin-bottom:30%;
-  width: 200px;
-  height:50px;
-  font-size: medium;
-  background-color:${colors.text};
-  border-radius: 8px; 
-  color: white;
-  transition-duration: 0.4s;
-  padding: 5px;
-}
+  .boton1 {
+    margin-bottom: 30%;
+    width: 200px;
+    height: 50px;
+    font-size: medium;
+    background-color: ${colors.text};
+    border-radius: 8px;
+    color: white;
+    transition-duration: 0.4s;
+    padding: 5px;
+  }
 
-.boton2{
-  width: 200px;
-  height:50px;
-  font-size: medium;
-  background-color:${colors.buttoncarrousel};
-  border-radius: 8px; 
-  color: white;
-  transition-duration: 0.4s;
-  padding: 5px;
-  margin-left:90%;
-}
+  .boton2 {
+    width: 200px;
+    height: 50px;
+    font-size: medium;
+    background-color: ${colors.buttoncarrousel};
+    border-radius: 8px;
+    color: white;
+    transition-duration: 0.4s;
+    padding: 5px;
+    margin-left: 90%;
+  }
 
-.boton3{
-  width: 200px;
-  height:50px;
-  font-size: medium;
-  background-color:${colors.color2};
-  border-radius: 8px; 
-  color: white;
-  transition-duration: 0.4s;
-  margin-bottom:30%;
-  margin-left:80%;
-}
-`
+  .boton3 {
+    width: 200px;
+    height: 50px;
+    font-size: medium;
+    background-color: ${colors.color2};
+    border-radius: 8px;
+    color: white;
+    transition-duration: 0.4s;
+    margin-bottom: 30%;
+    margin-left: 80%;
+  }
+`;
 
-//sobre nosotros 
+//sobre nosotros
 export const About = styled.section`
-display: flex;
-justify-content: space-between;
-align-items: center;
-text-align: left;
-padding: 2%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  text-align: left;
+  padding: 2%;
+  margin-left: 40px;
 
-.text{
-  width:20%;
-  display: inline-block;
-  order: 1;
-  flex-grow: 1;
-}
+  .text {
+    width: 20%;
+    display: inline-block;
+    order: 1;
+    flex-grow: 1;
+  }
 
-h4{
-  font-size: xx-large;
-  color:${colors.text};
-  padding-right:30px;
-}
+  h4 {
+    font-size: xx-large;
+    color: ${colors.text};
+    padding-right: 30px;
+  }
 
-p{
-  font-size: x-large;
-  color:${colors.text};
-  padding-right:30px;
-}
+  p {
+    font-size: x-large;
+    color: ${colors.text};
+    padding-right: 30px;
+  }
 
-button{
-  width: 300px;
-  height:50px;
-  font-size: medium;
-  background-color:${colors.color2};
-  border-radius: 8px;
-  border:2px solid #FA8184; 
-  color: white;
-  transition-duration: 0.4s;
-  padding: 10px;
-}
+  button {
+    width: 300px;
+    height: 50px;
+    font-size: medium;
+    background-color: ${colors.color2};
+    border-radius: 8px;
+    border: 2px solid #fa8184;
+    color: white;
+    transition-duration: 0.4s;
+    padding: 10px;
+  }
 
-
-.imageabout{
-  width: 55%;
-  margin-right: 50%;
-  display: inline-block;
-  order: 2;
-  flex-grow: 2;
-  padding: 100px;
-
-}
-`
+  .imageabout {
+    width: 55%;
+    margin-right: 50%;
+    display: inline-block;
+    order: 2;
+    flex-grow: 2;
+    padding: 100px;
+  }
+`;
 //nuestros servicios
 export const OurServices = styled.section`
-display: flex;
-justify-content: space-between;
-align-items: center;
-padding: 5%;
-background-color: ${colors.color1};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 5%;
+  background-color: ${colors.color1};
 
-.servicetext{
-color: ${colors.text};
-align-items: center;
-margin-left: 5%;
-font-size: large;
-}
+  .servicetext {
+    color: ${colors.text};
+    align-items: center;
+    margin-left: 5%;
+    font-size: large;
+  }
 
-h4{
-  font-size: xx-large;
-  text-align: center;
-}
+  h4 {
+    font-size: xx-large;
+    text-align: center;
+  }
 
-p{
-  font-size: large;
-  text-align: center;
-  margin-left:20%;
-  margin-right:20%;
-}
-`
+  p {
+    font-size: large;
+    text-align: center;
+    margin-left: 20%;
+    margin-right: 20%;
+  }
+`;
 //botones servicios
 export const IconsServices = styled.section`
-display: flex;
-align-items: center;
-background-color: ${colors.color1};
-flex-flow: nowrap;
-justify-content: space-between;
-flex-direction: row;
-padding-bottom:0px;
-padding-top:0px;
+  display: flex;
+  background-color: ${colors.color1};
+  width: 100%;
 
-.beautyicon{
-  order:1;
-  flex-grow:1;
-}
-.nutritionicon{
-  order:2;
-  flex-grow:1;
-}
+  .beautyicon {
+    order: 1;
+    flex-grow: 1;
+  }
+  .nutritionicon {
+    order: 2;
+    flex-grow: 1;
+  }
 
-.workouticon{
-  order:3;
-  flex-grow:1;
-}
-`
+  .workouticon {
+    order: 3;
+    flex-grow: 1;
+  }
+`;
 //servicios textos
 export const TextServices = styled.section`
-display: flex;
-align-items: center;
-background-color: ${colors.color1};
-flex-flow: wrap;
-justify-content: space-evenly;
-flex-direction: row;
-color: ${colors.text};
-padding:0px;
-margin-top:0px;
+  display: flex;
+  background-color: ${colors.color1};
+  color: ${colors.text};
+  width: 100%;
 
-.textbeauty{
-  order:1;
-  flex-grow:1;
+  .textbeauty {
+    order: 1;
+    flex-grow: 1;
+  }
 
-}
+  .textnutrition {
+    order: 2;
+    flex-grow: 1;
+  }
 
-.textnutrition{
-  order:2;
-  flex-grow:1;
+  .textworkout {
+    order: 3;
+    flex-grow: 1;
+  }
 
-}
+  h4 {
+    font-size: xx-large;
+    text-align: center;
+  }
 
-.textworkout{
-  order:3;
-  flex-grow:1;
+  p {
+    font-size: large;
+    text-align: center;
+  }
 
-}
-
-h4{
-  font-size: xx-large;
-  text-align: center;
-}
-
-p{
-  font-size: large;
-  text-align: center;
-}
-`
+  .button {
+  }
+`;
 //comentarios
 export const Grades = styled.section`
-display: flex;
-flex-direction:column;
-background-color: ${colors.testimoniesback};
-text-align:center;
-color: ${colors.text};
-
-img{
-  width: 100%;
-}
-
-h4{
-  font-size: xx-large;
+  display: flex;
+  flex-direction: column;
+  background-color: ${colors.testimoniesback};
   text-align: center;
-}
+  color: ${colors.text};
 
-p{
-  font-size: large;
-  text-align: center;
-  margin-left:30%;
-  margin-right:30%;
-}
-`
+  img {
+    width: 100%;
+  }
+
+  h4 {
+    font-size: xx-large;
+    text-align: center;
+  }
+
+  p {
+    font-size: large;
+    text-align: center;
+    margin-left: 30%;
+    margin-right: 30%;
+  }
+`;
 
 //form
 export const SignForm = styled.section`
-display: flex;
-align-items: center;
-flex-direction:column;
-justify-content: space-between;
-background-color: white;
-text-align:center;
-align-content:center;
-color: ${colors.text};
-flex-wrap: wrap
-
-h2{
-  font-size: xxx-large;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: space-between;
+  background-color: white;
   text-align: center;
-}
+  align-content: center;
+  color: ${colors.text};
+  flex-wrap: wrap h2 {
+    font-size: xxx-large;
+    text-align: center;
+  }
 
-p{
-  font-size: large;
-  text-align: center;
-  margin-left:35%;
-  margin-right:35%;
-}
+  p {
+    font-size: large;
+    text-align: center;
+    margin-left: 35%;
+    margin-right: 35%;
+  }
 
-input{
-  width: 90%;
-  background-color: ${colors.color1};
-  border: 2px solid ${colors.color1};
-  border-radius:8px;
-  color: ${colors.color2};
-  opacity:0.8;
-  font-size: large;
-  padding: 10px;
-  margin-bottom:30px;
+  input {
+    width: 90%;
+    background-color: ${colors.color1};
+    border: 2px solid ${colors.color1};
+    border-radius: 8px;
+    color: ${colors.color2};
+    opacity: 0.8;
+    font-size: large;
+    padding: 10px;
+    margin-bottom: 30px;
+  }
 
-}
+  placeholder {
+    color: ${colors.color2};
+  }
 
-placeholder{
-  color: ${colors.color2};
-}
-
-input[type=submit] {
-  width: 300px;
-  height:50px;
-  font-size: medium;
-  background-color:${colors.color2};
-  border-radius: 8px;
-  border:2px solid #FA8184; 
-  color: white;
-  transition-duration: 0.4s;
-  padding: 10px;
-}
-`
+  input[type='submit'] {
+    width: 300px;
+    height: 50px;
+    font-size: medium;
+    background-color: ${colors.color2};
+    border-radius: 8px;
+    border: 2px solid #fa8184;
+    color: white;
+    transition-duration: 0.4s;
+    padding: 10px;
+  }
+`;
 // footer
 export const Styledfooter = styled.section`
-display: flex;
-justify-content: space-between;
-align-items: center;
-text-align: left;
-padding: 10%;
-background-color: ${colors.color2};
-color: white;
-
-.direction{
-  margin-left: 35%;
-}
-`
-export const Designfooter = styled.section`
-display: flex;
-justify-content:center;
-align-items:center;
-background-color: ${colors.copywrite};
-color:white;
-`
-//socialmedia
-export const Socialbar = styled.section`
-a {
-  text - decoration: none;
-  vertical - align: middle;
-  text - align: center;
-  line - height: 3;
-}
-
-p {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  text-align: left;
+  padding: 10%;
+  background-color: ${colors.color2};
   color: white;
-  position: relative;
-  left: -10px;
-  font - family: 'Lato';
-}
 
-#sidebar {
-  height: 250px;
-  width: 10px;
-  position: fixed;
-  text - align: center;
-  padding: 10px;
-  margin - left: 10px;
-}
-
-p i {
-  left: 30px;
-  position: relative;
-  vertical - align: middle;
-  text - align: center;
-  font - size: 24px;
-}
-
-.social {
-  margin - left: -200px;
-  width: 230px;
-  padding: 0;
-  display: inline - table;
-  height: 0px;
-  background - color: rgba(128, 128, 128, 0.73);
-  -moz - transition - property: margin - left;
-  -moz - transition - duration: 0.2s;
-  -moz - transition - delay: 0.2s;
-  -ms - transition - property: margin - left;
-  -ms - transition - duration: 0.2s;
-  -ms - transition - delay: 0.2s;
-  -o - transition - property: margin - left;
-  -o - transition - duration: 0.2s;
-  -o - transition - delay: 0.2s;
-  -webkit - transition - property: margin - left;
-  -webkit - transition - duration: 0.2s;
-  -webkit - transition - delay: 0.2s;
-  box - shadow: 0px 0px 6px 0px #3E3D3D;
-  cursor: pointer;
-}
-
-.social: hover {
-  margin - left: -30px;
-  width: 230px;
-  background - color: #3b5998;
-}
-
-.facebook: hover {
-  background - color: #3b5998;
-}
-
-.twitter: hover {
-  background - color: #4099FF;
-}
-
-.instagram: hover {
-  background - color: #4099FF;
-
-}
-`
+  .direction {
+    margin-left: 35%;
+  }
+`;
+export const Designfooter = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${colors.copywrite};
+  color: white;
+`;
