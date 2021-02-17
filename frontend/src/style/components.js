b7c23d1245c0002305a35bd840fc09ebeccfc49d
Choskas Delta <Choskas Delta>
2021-02-16 22:17:00 -0600

    modify style at style/components.js

--- frontend/src/style/components.js
+++ frontend/src/style/components.js
@@ -78,7 +78,7 @@ padding: 0 2rem;
 `;
 //carouselbootstrap
 export const CarouselStyled = styled.section`
-  width: 100vw;
+  width: 100%;
   max-width:1800px;
   display: flex;
   flex-wrap: nowrap;
